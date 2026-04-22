6d3e65bea4c835a64bb831aa3dd3e95b6704d567
Isaque Victor Duarte De Oliveira <45144815+109isaque10@users.noreply.github.com>
2026-04-22 14:47:18 -0300

    Update build.gradle.kts

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization")
+    alias(libs.plugins.kotlin.serialization)
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
 }
